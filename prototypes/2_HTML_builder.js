ecfc4c2225ab3969f8480fcee20b107ad2439865
guar47 <guar47@ya.ru>
2017-01-21 22:04:46 +0300

    add courses, rename folders, change eslint

---
+++ prototypes/2_HTML_builder.js
@@ -0,0 +1,37 @@
+const argTypes = {
+  body: a => typeof a === 'string',
+  children: a => a instanceof Array,
+  attrs: a => a instanceof Object,
+};
+
+const getArgName = arg =>
+  Object.keys(argTypes).filter(k => argTypes[k](arg))[0];
+
+const buildAttrString = attrs =>
+  Object.keys(attrs).map(key => ` ${key}="${attrs[key]}"`).join('');
+
+const buildHtml = tagArr => {
+  const tag = tagArr.slice(1)
+    .reduce((acc, arg) => ({ ...acc, [getArgName(arg)]: arg }),
+      { name: tagArr[0], attrs: {}, body: '', children: [] });
+
+  return [`<${tag.name}${buildAttrString(tag.attrs)}>`,
+    `${tag.body}${tag.children.map(buildHtml).join('')}`,
+    `</${tag.name}>`,
+  ].join('');
+};
+
+const data = ['html', [
+  ['meta', [
+    ['title', 'hello, hexlet!'],
+  ]],
+  ['body', { class: 'container' }, [
+    ['h1', { class: 'header' }, 'html builder example'],
+    ['div', [
+      ['span', 'span text2'],
+      ['span', 'span text3'],
+    ]],
+  ]],
+]];
+
+buildHtml(data);
